a365e52caa2dd655f07dd763d187419274a10476
Evan Hayes <evanhayes@Evans-MacBook-Pro.local>
2020-02-24 11:29:29 -0500

    Enter

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -3,6 +3,7 @@ import classes from './Toolbar.module.css';
 import NavigationItems from '../Navigationitems/Navigationitems.jsx';
 
 
+
 const Toolbar = () => (
  <header className={classes.Toolbar}>
      <div>
