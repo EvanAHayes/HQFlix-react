80d4db71d0d0efe12580762b7867ab486976fe33
Evan Hayes <evanhayes@Evans-MacBook-Pro.local>
2020-02-25 09:15:33 -0500

    Enter

--- src/containers/MovieBuilder/MovieBuilder.jsx
+++ src/containers/MovieBuilder/MovieBuilder.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Aux from '../../hoc/Aux';
+import classes from './MovieBuilder.module.css'
 
 class MovieBuilder extends Component{
 
     render(){
         return(
           <Aux>
+             <div className={classes.Body}>
 
+             </div>
           </Aux>
         );
     }
