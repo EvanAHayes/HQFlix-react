ba5d23dfa16133ca29b98232cfb37a244268bfdc
Evan Hayes <evanhayes@Evans-MacBook-Pro.local>
2020-02-25 14:33:03 -0500

    Enter

---
+++ src/components/UI/Carousel/Carousel.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';  
+import 'owl.carousel/dist/assets/owl.theme.default.css'; 
+
+
+const Carousel = (props) => (
+    <div>
+        <OwlCarousel items={3}
+                     className={"owl-theme"}
+                     loop
+                     nav
+                     margin={8} >
+                         
+        </OwlCarousel>
+    </div>
+)
+
+export default Carousel;
