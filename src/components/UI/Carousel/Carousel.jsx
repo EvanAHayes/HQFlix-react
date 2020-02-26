9b22cb3d0c2168f2201a4436d9dba2620143ab54
Evan Hayes <evanhayes@Evans-MacBook-Pro.local>
2020-02-26 12:18:44 -0500

    Enter

--- src/components/UI/Carousel/Carousel.jsx
+++ src/components/UI/Carousel/Carousel.jsx
@@ -3,6 +3,7 @@ import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import Demo from '../../../Images/demo.png';
 import Cloud from '../../../Images/cloud.jpg';
+import Logo from '../../../Images/logo.png';
 
 
 const MovieCarousel = (props) => (
@@ -11,15 +12,25 @@ const MovieCarousel = (props) => (
               slidesPerScroll={2}
               slidesPerPage={2}
               infinite
-              arrows
-              centered>
-                  
+              arrows>
+             <div>
              <img src={Demo} alt="Demo" />
+             <div>
+                 <h6>First slide</h6>
+             </div>
+             </div>
+             <div className={"Card-cover"}>
+             <img src={Cloud} alt="Demo" />
+             <div className={"Card-content"}>
+                 <h6>Second slide</h6>
+                 <span>Rating: <i className="fas fa-star"></i></span>
+             </div>
+             </div>
+             <img src={Demo} alt="Demo" />
+             <img src={Logo} alt="logo" />
              <img src={Cloud} alt="Demo" />
              <img src={Demo} alt="Demo" />
-             <img src={Cloud} alt="Demo" />
-             <img src={Demo} alt="Demo" />
-             <img src={Cloud} alt="Demo" />
+            
             
             
      </Carousel>
