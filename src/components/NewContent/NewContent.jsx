5ccef81fe9bce126ac7e1bbfa2dd5db76e34a4ec
Evan Hayes <evanhayes@Evans-MacBook-Pro.local>
2020-02-27 09:32:49 -0500

    Enter

--- src/components/NewContent/NewContent.jsx
+++ src/components/NewContent/NewContent.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './NewContent.module.css';
 
-const NewContent = () => (
+class NewContent extends Component {
+    render(){
+   return(
+    
   <div className={classes.Headcontent}>
- <div className={"container"}>
-<div className={"row"}>
-    <div className={"col-12"}>
+ <div class="container">
+<div class="row">
+    <div class="col-12">
         <h2>NewContent</h2>
-
+        <ul class="nav nav-tabs content--tabs" id="content--tabs" role="tablist">
+                  <li class="nav-item">
+                      <a class="nav-link active show" data-toggle="tab" href="#tab-1" role="tab" aria-controls="tab-1" aria-selected="true">New In Theaters</a>
+                  </li>  
+                  <li class="nav-item">
+                      <a class="nav-link" data-toggle="tab" href="#tab-2" role="tab" aria-controls="tab-2" aria-selected="false">Movies</a>
+                  </li>  
+                  <li class="nav-item">
+                      <a class="nav-link" data-toggle="tab" href="#tab-3" role="tab" aria-controls="tab-3" aria-selected="false">TV Shows</a>
+                  </li>  
+                </ul>
     </div>
 </div>
  </div>
+
   </div>
-);
+   );
+    }
+};
 
 export default NewContent;
