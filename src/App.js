e5a9da771eebf59e40f9a669913c9d0a091a567b
Evan Hayes <evanhayes@Evans-MBP.fios-router.home>
2020-08-06 17:53:48 -0400

    enter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Layout from './containers/Layout/Layout';
 import MovieBuilder from './containers/MovieBuilder/MovieBuilder';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- src/components/LoginComponent/LoginComponent.jsx
+++ src/components/LoginComponent/LoginComponent.jsx
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
+import Button from '../UI/Button/Button';
 
  class Login extends Component {
+     
+    constructor(props){
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            hasLoginFailed: false,
+            ShowSuccessMessage: false
+        }
+    }
+
 
     render() {
         return (
             <div>
                 <div className="container">
                 <h1>Login</h1>
+                <Button>Create New User</Button>
                 <div class="form-group">
-           <label>Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+           <label>Username</label>
+          <input type="username" class="form-control" name="username" aria-describedby="emailHelp" value={this.state.username} />
        </div>
         <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" />
+        <label>Password</label>
+        <input type="password" class="form-control" name="password" value={this.state.password} />
        </div>
+        <Button>Submit</Button>
+       
                 </div>
             </div>
         )
