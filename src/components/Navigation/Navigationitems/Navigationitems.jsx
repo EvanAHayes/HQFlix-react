e4a019eeb520c349b6cc5102400ce4b52061b335
Evan Hayes <evanhayes@Evans-MBP.fios-router.home>
2020-10-09 14:13:32 -0400

    ENTER

--- src/components/Navigation/Navigationitems/Navigationitems.jsx
+++ src/components/Navigation/Navigationitems/Navigationitems.jsx
@@ -3,25 +3,25 @@ import NavItem from '../Navitems/Navitems';
 import classes from './NavigationItems.module.css';
 import { Component } from 'react';
 import AuthenticationService from '../../Authentication/AuthenticationService.js';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 
-class navigationItems extends Component{
+class navigationItems extends Component {
 
-render(){
+   render() {
 
-    const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
-    let username = AuthenticationService.getLoggedInUserName();
-    
- return(
-    <ul className={classes.NavigationItems}>
-        <NavItem link="/">Home</NavItem>
-        {!isUserLoggedIn && <NavItem link="/SignIn">Sign-In</NavItem>}
-        {isUserLoggedIn && <NavItem link={`/${username}/favorites`}>Favorites</NavItem>}
-        {isUserLoggedIn && <NavItem link="/logout" clicked={AuthenticationService.logout}>LogOut</NavItem>}
-    </ul>
- )
-}
+      const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
+      let username = AuthenticationService.getLoggedInUserName();
+
+      return (
+         <ul className={classes.NavigationItems}>
+            <NavItem link="/">Home</NavItem>
+            {!isUserLoggedIn && <NavItem link="/SignIn">Sign-In</NavItem>}
+            {isUserLoggedIn && <NavItem link={`/${username}/favorites`}>Favorites</NavItem>}
+            {isUserLoggedIn && <NavItem link="/logout" clicked={AuthenticationService.logout}>LogOut</NavItem>}
+         </ul>
+      )
+   }
 }
 
 export default withRouter(navigationItems);
