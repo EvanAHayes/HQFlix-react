da444769faa75129171b11166d876b0ef617e9df
Evan Hayes <evanhayes@Evans-MBP.fios-router.home>
2020-10-09 14:14:00 -0400

    enter

--- src/components/Authentication/AuthenticationService.js
+++ src/components/Authentication/AuthenticationService.js
@@ -2,25 +2,25 @@ import axios from 'axios';
 
 class AuthenticationService {
 
-    createJWTToken(token){
+    createJWTToken(token) {
         return 'Bearer ' + token
-       }
+    }
 
-    createBasicAuthToken(Username,Password){
+    createBasicAuthToken(Username, Password) {
         return 'Basic ' + window.btoa(Username + ":" + Password)
     }
 
-    registerSuccessfullLogin(username,password){
+    registerSuccessfullLogin(username, password) {
         console.log('register')
         sessionStorage.setItem('authenticatedUser', username);
     }
 
     //Todo: dont forget to add logout button for this
-    logout(){
+    logout() {
         sessionStorage.removeItem('authenticatedUser')
     }
 
-    isUserLoggedIn(){
+    isUserLoggedIn() {
         let user = sessionStorage.getItem('authenticatedUser')
         if (user === null) {
             return false
@@ -28,38 +28,40 @@ class AuthenticationService {
         return true;
     }
 
-    getLoggedInUserName(){
+    getLoggedInUserName() {
         let user = sessionStorage.getItem('authenticatedUser');
-        if(user === null){
-         return ""
-        } 
-        
+        if (user === null) {
+            return ""
+        }
+
         return user;
     }
 
-    executeJwtAuthenticationService(username,password){
+    executeJwtAuthenticationService(username, password) {
 
-        return axios.post("http://localhost:8080/api/auth/login", 
-                {username, password}    
-                )}
+        return axios.post("http://localhost:8080/api/auth/login", {
+            username,
+            password
+        })
+    }
 
-    setupAxiosInterceptors(basicAuthHeader){    
-                    axios.interceptors.request.use(
-                        (config) => {
-                            if(this.isUserLoggedIn){
-                
-                            config.headers.authorization = basicAuthHeader
-                        }
-                        return config
-                    }
-                    )
-                
+    setupAxiosInterceptors(basicAuthHeader) {
+        axios.interceptors.request.use(
+            (config) => {
+                if (this.isUserLoggedIn) {
+
+                    config.headers.authorization = basicAuthHeader
                 }
+                return config
+            }
+        )
+
+    }
 
     registerSuccessFullLoginForJwt(username, token) {
-                    sessionStorage.setItem('authenticatedUser', username)
-                    this.setupAxiosInterceptors(this.createJWTToken(token))
-                }
+        sessionStorage.setItem('authenticatedUser', username)
+        this.setupAxiosInterceptors(this.createJWTToken(token))
+    }
 }
 
 export default new AuthenticationService()
--- src/containers/MovieBuilder/MovieBuilder.jsx
+++ src/containers/MovieBuilder/MovieBuilder.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Movie from '../../components/Movie/Movie';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import Layout from '../../containers/Layout/Layout';
 import LoginComponet from '../../components/LoginComponent/LoginComponent';
-import ErrorComponent from'../../components/ErrorComponent/ErrorComponent';
+import ErrorComponent from '../../components/ErrorComponent/ErrorComponent';
 import LogoutComponent from '../../components/LogoutComponent/LogoutComponent';
 import AuthenticationRoute from '../../components/Authentication/AuthenticationRoute';
 import NewUserComponent from '../../components/NewUserComponent/NewUserComponent';
@@ -13,28 +13,28 @@ import ListFavoritesComponent from '../../components/ListFavorites/ListFavorites
 
 
 
-class MovieBuilder extends Component{
+class MovieBuilder extends Component {
 
-    render(){
-        return(
-          <div>
-            <Router>
-              <Layout>
-                <Switch>
-                  <Route path="/" exact component={Movie} />
-                  <AuthenticationRoute path="/welcome/:name" component={Movie} />
-                  <Route exact path="/SignIn" component={LoginComponet} />
-                  <Route path="/register" component={NewUserComponent} /> 
-                  <AuthenticationRoute path="/:name/favorites" component={ListFavoritesComponent} />
-                  <AuthenticationRoute path="/logout" component={LogoutComponent} />
-                  <Route component={ErrorComponent} />
-                </Switch>
-              <Footer />
-              </Layout>
-            </Router>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Router>
+          <Layout>
+            <Switch>
+              <Route path="/" exact component={Movie} />
+              <AuthenticationRoute path="/welcome/:name" component={Movie} />
+              <Route exact path="/SignIn" component={LoginComponet} />
+              <Route path="/register" component={NewUserComponent} />
+              <AuthenticationRoute path="/:name/favorites" component={ListFavoritesComponent} />
+              <AuthenticationRoute path="/logout" component={LogoutComponent} />
+              <Route component={ErrorComponent} />
+            </Switch>
+            <Footer />
+          </Layout>
+        </Router>
+      </div>
+    );
+  }
 }
 
- export default MovieBuilder;
+export default MovieBuilder;
